98f5367433913ef21ea121a58a37dddd5a38655a
trpyy <trpttnbrh@gmail.com>
2023-01-16 20:49:06 +0200

    zf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!22");
+        System.out.println("Hello avraam");
+        System.out.println("Everyone be quiet");
+        System.out.println("Everyone quiet");
+        System.out.println("Everyone try");
+
     }
 }
